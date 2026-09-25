956e3f7269f5a93a2e83c1ee19fcfd722bfea8ca
zberrier <berri075@umn.edu>
2026-09-24 20:26:57 -0500

    Update postgres.sql

--- project1/postgres.sql
+++ project1/postgres.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS comments;
 DROP TABLE IF EXISTS links;
 DROP TABLE IF EXISTS posts;
+DROP TABLE IF EXISTS locations;
 DROP TABLE IF EXISTS accounts;
 
 CREATE TABLE accounts (
@@ -9,18 +10,25 @@ CREATE TABLE accounts (
     password TEXT NOT NULL
 );
 
+CREATE TABLE locations (
+    location_id SERIAL PRIMARY KEY,
+    location TEXT,
+    lat FLOAT NOT NULL,
+    long FLOAT NOT NULL
+);
+
 CREATE TABLE posts (
     post_id SERIAL PRIMARY KEY,
     author TEXT,    -- author can be username or NULL (anonymous)
     account INT NOT NULL,
-    lat FLOAT,      -- Is location optional input? If so, then NULL values possible
-    long FLOAT,
+    place INT UNIQUE,
     title TEXT NOT NULL,
     time TIMESTAMP DEFAULT NOW(),
     description TEXT NOT NULL,
     links INT UNIQUE,
     comments INT UNIQUE,
-    FOREIGN KEY (account) REFERENCES accounts(account_id)
+    FOREIGN KEY (account) REFERENCES accounts(account_id),
+    FOREIGN KEY (place) REFERENCES locations(location_id)
 );
 
 CREATE TABLE links (
